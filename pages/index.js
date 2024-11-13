81a1e50dd95b07ed5ce430e448ff3298e305d19a
Adriano de Alencar <109616027+Alencar7@users.noreply.github.com>
2024-11-13 16:09:25 +0000

    aula4-completa

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    //return <h1> Teste do teste </h1>
+    return <h1> 
+        oi, minha vida.
+        Voce e a mulher mais linda e bela do mundo! 
+        Eu te amo pra sempre! 
+        </h1>
+}
+
+export default Home;
