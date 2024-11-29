d6671516c99ca2e9115b230ff265b4436cc659dd
Adriano de Alencar <109616027+Alencar7@users.noreply.github.com>
2024-11-29 19:21:21 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
     //return <h1> Teste do teste </h1>
     return <h1> 
-        oi, minha vida.
+        oi, minha vida. s2
         Voce e a mulher mais linda e bela do mundo! 
-        Eu te amo pra sempre! 
-        </h1>
+        Eu te amo pra sempre! </h1>
+        
 }
 
 export default Home;
