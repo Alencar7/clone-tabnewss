bc1b905ef8958ec6dfe822ec04828672338a70d6
Adriano de Alencar <109616027+Alencar7@users.noreply.github.com>
2024-12-02 22:53:23 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 function Home() {
-    //return <h1> Teste do teste </h1>
-    return <h1> 
-        oi, minha vida. s2
-        Voce e a mulher mais linda e bela do mundo! 
-        Eu te amo pra sempre! </h1>
-        
+  return;
+  <h1>
+    oi, minha vida. s2 Voce e a mulher mais linda e bela do mundo! Eu te amo pra
+    sempre!
+  </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
